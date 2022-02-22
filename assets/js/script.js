75d64449c9ae482fa0c8746ad0039e4ebf50a10b
Ramu Makkena <MRamuMailer@gmail.com>
2022-02-22 15:25:18 -0600

    capturing form input to add a new task

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,14 @@ formEl.addEventListener("submit", createTaskHandler);
 function createTaskHandler(event) {
     event.preventDefault();
     var listItemEl = document.createElement("li");
-    listItemEl.textContent = "This is sampel";
     listItemEl.className="task-item";
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    var taskInfoEL = document.createElement("div");
+    taskInfoEL.className="task-info";
+    taskInfoEL.innerHTML="<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>"+taskTypeInput+"</span>";
+    listItemEl.appendChild(taskInfoEL);
     tasksToDoEl.appendChild(listItemEl);
 }
