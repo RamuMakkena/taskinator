dd6a3b4bb99b95bc28ccf54bb90b3535529ace4d
Ramu Makkena <MRamuMailer@gmail.com>
2022-02-22 16:17:59 -0600

    Form validation for adding a task

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,16 @@ createTaskEl = (taskdataObj) => {
     taskInfoEL.innerHTML="<h3 class='task-name'>" + taskdataObj.name + "</h3><span class='task-type'>"+taskdataObj.type+"</span>";
     listItemEl.appendChild(taskInfoEL);
     tasksToDoEl.appendChild(listItemEl);
+    formEl.reset();
 }
 function taskFormHandler(event) {
-    
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    if(!taskNameInput || !taskTypeInput){
+        alert("You need to fill out the task form!");
+        return;
+    }
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
